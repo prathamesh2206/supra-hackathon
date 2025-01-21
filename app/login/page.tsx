d66aed5102e8bcc52192fd8c018a193ba564abcd
Om Kute <kuteom2006@gmail.com>
2025-01-21 10:44:02 +0530

    Added API Route

---
+++ app/login/page.tsx
@@ -0,0 +1,65 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card";
+  import { Input } from "@/components/ui/input";
+  import { Label } from "@/components/ui/label";
+  import { auth, signIn } from "../utils/auth";
+  import { redirect } from "next/navigation";
+  import { SubmitButton } from "../components/SubmitButton";
+  
+  export default async function login() {
+    const session = await auth();
+  
+    if (session?.user) {
+      redirect("/dashboard");
+    }
+  
+    return (
+      <>
+        <div
+          className="
+                  flex
+                  h-screen
+                  w-full
+                  items-center
+                  justify-center
+                  px-4
+              "
+        >
+          <Card className="max-w-sm">
+            <CardHeader>
+              <CardTitle className="text-2xl">Login</CardTitle>
+              <CardDescription>
+                Enter your email below to login into your account
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <form
+                action={async (formData) => {
+                  "use server";
+                  await signIn("nodemailer", formData);
+                }}
+                className="flex flex-col gap-y-4"
+              >
+                <div className="flex flex-col gap-y-2">
+                  <Label>Email</Label>
+                  <Input
+                    name="email"
+                    type="email"
+                    required
+                    placeholder="invoicex@gmail.com"
+                  ></Input>
+                </div>
+                <SubmitButton text="Login" />
+              </form>
+            </CardContent>
+          </Card>
+        </div>
+      </>
+    );
+  }
+  
